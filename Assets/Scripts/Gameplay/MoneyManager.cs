def435c9082a74cb424c6f80e973bf0eae729a80
Alexander Thomas <s179069@students.aie.edu.au>
2018-07-23 11:15:29 -0700

    Add MoneyManager

---
+++ Assets/Scripts/Gameplay/MoneyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour {
+
+	public MoneyManager Instance { get; private set; }
+
+    public float startingMoney;
+
+    public float Money { get; private set; }
+
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AddMoney(float amt)
+    {
+        Money += amt;
+    }
+
+    public void SubtractMoney(float amt)
+    {
+        Money -= amt;
+    }
+
+}
