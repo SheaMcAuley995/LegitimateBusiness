2e8c9ec5a1e0fa4ecd115be7507bd9154a767e84
Alexander Thomas <s179069@students.aie.edu.au>
2018-07-23 14:26:53 -0700

    Add object placement stage countdown timer

---
+++ Assets/Scripts/Gameplay/RoundManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour {
+
+    public delegate void OnStageChange();
+    private OnStageChange onTimerStart;
+    private OnStageChange onTimerFinish;
+    private OnStageChange onPizzaReached;
+
+    public float placementTime;
+
+    public float placementTimer { get; private set; }
+
+
+
+    private void Start()
+    {
+        StartRound();
+    }
+
+    private void Update()
+    {
+        if(placementTimer > 0)
+        {
+            placementTimer -= Time.deltaTime;
+            if(placementTimer <= 0 && onTimerFinish != null)
+            {
+                onTimerFinish();
+            }
+        }
+    }
+
+    private void StartRound()
+    {
+        placementTimer = placementTime;
+        if(onTimerStart != null)
+        {
+            onTimerStart();
+        }
+    }
+
+    public void AddOnTimerStart(OnStageChange func)
+    {
+        if(onTimerStart == null)
+        {
+            onTimerStart = func;
+        }
+        else
+        {
+            onTimerStart += func;
+        }
+    }
+
+    public void AddOnTimerFinish(OnStageChange func)
+    {
+        if (onTimerFinish == null)
+        {
+            onTimerFinish = func;
+        }
+        else
+        {
+            onTimerFinish += func;
+        }
+    }
+
+    public void AddOnPizzaReached(OnStageChange func)
+    {
+        if (onPizzaReached == null)
+        {
+            onPizzaReached = func;
+        }
+        else
+        {
+            onPizzaReached += func;
+        }
+    }
+
+}
