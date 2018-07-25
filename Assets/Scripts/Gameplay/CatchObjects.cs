606cb78fee0975857f2775c4fd2cdcd698986d48
Alexander Thomas <s179069@students.aie.edu.au>
2018-07-25 09:36:19 -0700

    Add CatchObject script

---
+++ Assets/Scripts/Gameplay/CatchObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchObjects : MonoBehaviour {
+
+    public Transform resetPos;
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("PlacableObjects"))
+        {
+            collision.transform.position = resetPos.position;
+        }
+    }
+
+}
